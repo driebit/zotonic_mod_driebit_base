8924ecdb3cc0154f8974cf064b5d64febdff948b
Marc Worrell <marc@worrell.nl>
2026-01-29 22:49:22 +0100

    filter background_position: take into account the 1.x medium_edit_settings. Fixes #1

--- src/filters/filter_background_position.erl
+++ src/filters/filter_background_position.erl
@@ -3,17 +3,25 @@
 
 -export([background_position/2]).
 
-% -include_lib("zotonic_core/include/zotonic.hrl").
-
 -spec background_position(integer(), z:context()) -> binary().
 background_position(Id, Context) ->
-    case m_rsc:p(Id, <<"crop_center">>, Context) of
-        undefined ->
+    case m_rsc:p(Id, <<"medium_edit_settings">>, Context) of
+        #{
+            % <<"crop_center_x">> := X,
+            % <<"crop_center_y">> := Y
+        } ->
+            % Cropping is done by the image resize routines, so we
+            % can center the image.
             <<"center center">>;
-        <<>> ->
-            <<"center center">>;
-        CropCenter ->
-            get_position(Id, CropCenter, Context)
+        _ ->
+            case m_rsc:p(Id, <<"crop_center">>, Context) of
+                undefined ->
+                    <<"center center">>;
+                <<>> ->
+                    <<"center center">>;
+                CropCenter ->
+                    get_position(Id, CropCenter, Context)
+            end
     end.
 
 get_position(Id, CropCenter, Context) ->
@@ -23,7 +31,7 @@ get_position(Id, CropCenter, Context) ->
                 #{
                     <<"width">> := Width,
                     <<"height">> := Height
-                } when is_integer(Width), is_integer(Height) ->
+                } when is_number(Width), is_number(Height) ->
                     try
                         get_background_position(z_convert:to_integer(X), z_convert:to_integer(Y), Width, Height, Context)
                     catch _:_ ->
@@ -36,44 +44,22 @@ get_position(Id, CropCenter, Context) ->
             <<"center center">>
     end.
 
-get_background_position(_X, _Y, undefined, _Height, _Context) ->
-    <<"center center">>;
-get_background_position(_X, _Y, _Width, undefined, _Context) ->
-    <<"center center">>;
-get_background_position(_X, _Y, undefined, undefined, _Context) ->
-    <<"center center">>;
-get_background_position(_X, _Y, 0, _Height, _Context) ->
-    <<"center center">>;
-get_background_position(_X, _Y, _Width, 0, _Context) ->
-    <<"center center">>;
 get_background_position(X, Y, Width, Height, _Context) ->
     PX = get_x(X, Width),
     PY = get_y(Y, Height),
     <<PX/binary, " ", PY/binary>>.
 
-get_x(X, Width)->
-    Percentage = (X/Width)*100,
-    case Percentage < 34 of
-        true ->
-            <<"left">>;
-        false ->
-            case Percentage > 66 of
-                true ->
-                    <<"right">>;
-                false ->
-                    <<"center">>
-            end
-    end.
-get_y(Y, Height)->
-    Percentage = (Y/Height)*100,
-    case Percentage < 34 of
-        true ->
-            <<"top">>;
-        false ->
-            case Percentage > 66 of
-                true ->
-                    <<"bottom">>;
-                false ->
-                    <<"center">>
-            end
-    end.
+get_x(_X, 0)-> <<"center">>;
+get_x(X, Width)-> get_xp(X/Width).
+
+get_xp(Percentage) when Percentage < 0.34 -> <<"left">>;
+get_xp(Percentage) when Percentage > 0.66 -> <<"right">>;
+get_xp(_) -> <<"center">>.
+
+get_y(_Y, 0) -> <<"center">>;
+get_y(Y, Height)-> get_yp(Y/Height).
+
+get_yp(Percentage) when Percentage < 0.34 -> <<"top">>;
+get_yp(Percentage) when Percentage > 0.66 -> <<"bottom">>;
+get_yp(_) -> <<"center">>.
+
